32134517829b57148195af849c1e07cfa03026a7
Bazaraa <bazardaginad1996@gmail.com>
2020-12-10 23:09:52 +0800

    Togloomiin onoog 100 bolgoh

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
     document.getElementById('score-'+activePlayer).textContent =scores[activePlayer];
 
     // Уг тоглогч хожсон эсэхийг шалгах
-    if(scores[activePlayer] >= 10) {
+    if(scores[activePlayer] >= 100) {
         // тоглоомыг дууссан төлөвт оруулна
         isNewGame= false;
         //Та хожлоо. БАЯР ХҮРГЭЕ. Алив ахдаа дансаа. г нэрний оронд гаргана
